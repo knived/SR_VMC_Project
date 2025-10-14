0ddd46ad16a83eba96f75f54e17dbad8b708e41b
knived <devin.karia@gmail.com>
2025-10-14 16:59:51 +0100

    working vispa

--- mich/vispa_test.py
+++ mich/vispa_test.py
@@ -1,18 +1,16 @@
+import os
 import pybullet as p
 import pybullet_data
-import time
-import math
-from datetime import datetime
 
-# connect to server
-physicsClient = p.connect(p.GUI)
+p.connect(p.GUI)
 
-# load models
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,0)
-planeId = p.loadURDF("plane.urdf", [0,0,-1], useFixedBase=True)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+planeId = p.loadURDF("plane.urdf", [0,0,-5], useFixedBase=True)
 
-startPos = [0, 0, 0]
-startOrientation = p.getQuaternionFromEuler([0, 0, 0])
-robot = p.loadURDF("summer/urdf/r2d2.urdf.xacro", startPos, startOrientation,
-                   useFixedBase=True)
+robot = p.loadURDF("mich/VispaSat.urdf", useFixedBase=False)
+
+while 1:
+    p.stepSimulation()
+
+p.disconnect()
